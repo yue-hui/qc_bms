d9310006217be8381fd48894ce4145922aab1ba3
yexl <1763277307@qq.com>
2019-01-30 22:09:56 +0800

    test123456

--- gelei-guard-bms/config/dev.env.js
+++ gelei-guard-bms/config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   IS_SIGN: true,
   TRANSFER_REQ: '"https://msdev.dev.zhixike.net/greenguard"',
-  BASE_API: '"/api"',
+  BASE_API: '"/gelei-guard-bms/api"',
   GNW_APPID: '"437EC0AC7F0000015E2BBF4849643C96"',
   GNW_TOKEN: '"27688ab70a56db714b59a6ebc79b8509a1f81629ce8edc743e1bc23e24465735"',
   GNW_APP_TOKEN: '"eyJhbGciOiJIUzI1NiJ9.eyJBUFBfVE9LRU46IjoiMTU5NTkyNzY2ODZfeUJGNVFoIiwiZXhwIjoxNTQ3MTkzMTE5fQ.NkiDvuo2lLWRm-4J2PTr8y5bpjmWrhyPUT0VQs-Y0b4"',
--- gelei-guard-bms/config/prod.env.js
+++ gelei-guard-bms/config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   IS_SIGN: true,
   TRANSFER_REQ: '"https://ms.gwchina.cn/greenguard"',
-  BASE_API: '"/api"',
+  BASE_API: '"/gelei-guard-bms/api"',
   GNW_APPID: '"437EC0AC7F0000015E2BBF4849643C96"',
   GNW_TOKEN: '"27688ab70a56db714b59a6ebc79b8509a1f81629ce8edc743e1bc23e24465735"',
   GNW_APP_TOKEN: '"eyJhbGciOiJIUzI1NiJ9.eyJBUFBfVE9LRU46IjoiMTU5NTkyNzY2ODZfeUJGNVFoIiwiZXhwIjoxNTQ3MTkzMTE5fQ.NkiDvuo2lLWRm-4J2PTr8y5bpjmWrhyPUT0VQs-Y0b4"',
