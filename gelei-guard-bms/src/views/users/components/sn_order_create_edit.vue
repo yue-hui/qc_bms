32fcab7f1bcef12ff20a24451f00d90250992709
8696 <204084802@qq.com>
2023-05-19 13:36:15 +0800

    sn

--- gelei-guard-bms/src/views/users/components/sn_order_create_edit.vue
+++ gelei-guard-bms/src/views/users/components/sn_order_create_edit.vue
@@ -255,11 +255,11 @@ export default {
             this.form.userId=res.data.userId
             this.form.payType="0"
           }
-          // else{
-          //   this.form.orderNo=''
-          //   this.form.parentName=''
-          //   this.form.userId=''
-          // }
+          else{
+            this.form.orderNo=''
+            // this.form.parentName=''
+            this.form.userId=''
+          }
         } else {
           this.$message.error(res.message)
         }
